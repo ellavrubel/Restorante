9740f9a1882b43fd74f346e4de05d8e84c4712f5
ellavrubel <webella@protonmail.com>
2019-12-31 15:45:46 +0300

    futureItems finished

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
 $(document).ready(function () {
+
+   // Modal Windows
+
    $('a.LoginModal, a.header__list-item, a.header__list-item_btn').click(function (e) {
        e.preventDefault();
         var destinationPopup = $(this).attr('href');
@@ -7,6 +10,31 @@ $(document).ready(function () {
         $('.popup__close').click(function () {
            $(destinationPopup).removeClass('window');
         });
-
    });
+
+    // Carousel
+
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:20,
+        dots: false,
+        nav:true,
+        navText:[
+            '<img src="img/FT_left-arrow.png" alt="Prev">',
+            '<img src="img/FT_right-arrow.png" alt="Next">'
+        ],
+        slideBy: 6,
+        smartSpeed: 1200,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:6
+            }
+        }
+    })
 });
